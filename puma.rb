fce4463c3b3f8296ec8fff5193b69a25e8fb0a40
wakana.miura <wakana.miura@supership.jp>
2024-10-03 20:52:26 +0900

    Generate puma config
    $ bundle exec cap produciton puma.rb
    
    デプロイ先にアップロードされたものを持ってきた

---
+++ puma.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env puma
+
+directory '/var/www/app/current'
+rackup "/var/www/app/current/config.ru"
+environment 'production'
+
+tag ''
+
+pidfile "/var/www/app/shared/tmp/pids/puma.pid"
+state_path "/var/www/app/shared/tmp/pids/puma.state"
+stdout_redirect '/var/www/app/shared/log/puma_access.log', '/var/www/app/shared/log/puma_error.log', true
+
+
+threads 0,16
+
+
+
+bind 'unix:///var/www/app/shared/tmp/sockets/puma.sock'
+
+workers 0
+
+
+
+
+restart_command 'bundle exec puma'
+
+
+prune_bundler
+
+
+on_restart do
+    puts 'Refreshing Gemfile'
+      ENV["BUNDLE_GEMFILE"] = ""
+end
